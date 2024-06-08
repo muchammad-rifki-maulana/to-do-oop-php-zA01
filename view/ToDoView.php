3215eddc09761e8886a396e8a86e02a6dc95cf6d
Muchammad Rifki Maulana <mrifkimaulana1998@gmail.com>
2024-06-08 12:59:05 +0700

    update view/ToDoView.php

--- view/ToDoView.php
+++ view/ToDoView.php
@@ -2,10 +2,43 @@
 
 namespace View;
 
+use Helper\InputHelper;
+use Service\ToDoService;
+
 class ToDoView
 {
+    public function __construct(private ToDoService $toDoService)
+    {
+    }
+
     public function showToDo(): void
     {
+        while (true) {
+            echo "to do\n";
+
+            $this->toDoService->showToDo();
+
+            echo "menu\n";
+            echo "1. add\n";
+            echo "2. remove\n";
+            echo "x. exit\n";
+
+            $choose = InputHelper::input("choose");
+
+            switch (strtolower($choose)) {
+                case "1":
+                    $this->addToDo();
+                    break;
+                case "2":
+                    $this->removeToDo();
+                    break;
+                case "x":
+                    echo "\n- see you -\n";
+                    exit;
+                default:
+                    echo "\n- option not understand -\n\n";
+            }
+        }
     }
 
     public function addToDo(): void
