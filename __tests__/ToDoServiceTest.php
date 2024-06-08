83b33ee207e4559a0f2c8cc602a16246043f01af
Muchammad Rifki Maulana <mrifkimaulana1998@gmail.com>
2024-06-08 10:42:59 +0700

    add __tests__/ToDoServiceTest.php

---
+++ __tests__/ToDoServiceTest.php
@@ -0,0 +1,26 @@
+<?php
+
+require_once __DIR__ . "/../entity/ToDo.php";
+require_once __DIR__ . "/../repository/ToDoRepository.php";
+require_once __DIR__ . "/../service/ToDoService.php";
+
+use Entity\ToDo;
+use Repository\ToDoRepositoryImpl;
+use Service\ToDoServiceImpl;
+
+function testShowToDo(): void
+{
+    $toDoRepository = new ToDoRepositoryImpl();
+    $toDoService = new ToDoServiceImpl($toDoRepository);
+
+    $toDoService->showToDo();
+
+    // don't forget if you want to test $toDo make it public
+    $toDoRepository->toDo[] = new ToDo("a");
+    $toDoRepository->toDo[] = new ToDo("b");
+    $toDoRepository->toDo[] = new ToDo("c");
+
+    $toDoService->showToDo();
+}
+
+testShowToDo();
