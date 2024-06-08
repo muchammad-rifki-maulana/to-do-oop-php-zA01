272f317a28edb312cc168391a3771a4b76610c4d
Muchammad Rifki Maulana <mrifkimaulana1998@gmail.com>
2024-06-08 13:06:34 +0700

    add __tests__/ToDoViewTest.php

---
+++ __tests__/ToDoViewTest.php
@@ -0,0 +1,20 @@
+<?php
+
+require_once __DIR__ . "/../entity/ToDo.php";
+require_once __DIR__ . "/../repository/ToDoRepository.php";
+require_once __DIR__ . "/../service/ToDoService.php";
+require_once __DIR__ . "/../helper/InputHelper.php";
+require_once __DIR__ . "/../view/ToDoView.php";
+
+use Repository\ToDoRepositoryImpl;
+use Service\ToDoServiceImpl;
+use View\ToDoView;
+
+function testViewShowToDo(): void
+{
+    $toDoView = new ToDoView(new ToDoServiceImpl(new ToDoRepositoryImpl()));
+
+    $toDoView->showToDo();
+}
+
+testViewShowToDo();
